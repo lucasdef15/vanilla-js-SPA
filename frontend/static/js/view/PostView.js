f3a7b42880c57920d0f94324d360fcde03fe1ac7
lucas <lucasff15@hotmail.com>
2023-06-26 15:07:16 -0300

    modified:   frontend/static/js/index.js 	new file:   frontend/static/js/view/PostView.js 	modified:   frontend/static/js/view/Posts.js

--- frontend/static/js/view/PostView.js
+++ frontend/static/js/view/PostView.js
@@ -3,18 +3,17 @@ import AbstractView from './AbstractView.js';
 export default class extends AbstractView {
   constructor(params) {
     super(params);
-    this.setTitle('Posts');
+    this.setTitle('Viewing Post');
   }
 
   async getHtml() {
+    console.log(this.params.id);
     return `
       <h1>Posts</h1>
       <p>
           lorem ipsum dolor sit amet, consectetur adip e ea commodo sed do eiusmod tempor incididunt ut
       </p>
 
-      <a href="/posts/2" class="nav__link" data-link>view post</a>
-      
   `;
   }
 }
